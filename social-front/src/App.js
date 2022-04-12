8e90e242d2f122ed6c374ab73fa2438a961fb210
DaveXo9 <85836822+DaveXo9@users.noreply.github.com>
2022-04-12 13:20:35 +0200

    Add files via upload

---
+++ social-front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import * as React from 'react';
+import Navbar from './Navbar';
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;

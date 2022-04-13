597eec8c01943c08abb39c4c6b571ec240b361e0
CvetkovskiIvor <cvetkovskiivor@gmail.com>
2022-04-13 19:21:11 +0200

    Added cover and profile photos

--- social-front/src/App.js
+++ social-front/src/App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div>
      <Navbar/>
+     <Profile/>
     </div>
   );
 }
---
+++ social-front/src/components/Profile/Profile.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import './index.css';
+import CoverPhoto from '../../Images/profile-cover.png';
+import ProfilePhoto from '../../Images/profile-image.png';
+
+export default function Profile() {
+    
+  return (
+      <div className="Profile">
+          <Box className='profile-container'>
+            <img className='profile-cover-photo' src={CoverPhoto} alt=''></img>
+            <Box className='profile-image-container'>
+              <img className='profile-image-photo' src={ProfilePhoto} alt=''></img>
+              
+            </Box>
+          </Box>
+      </div>
+  );
+}

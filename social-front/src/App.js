1e4925006b79085612ab70ad837dbc20eff112f0
CvetkovskiIvor <cvetkovskiivor@gmail.com>
2022-04-16 19:57:13 +0200

    Fix merge conflict

--- social-front/src/App.js
+++ social-front/src/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import CommunityHeader from './components/CommunityHeader/CommunityHeader';
 import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div>
      <Navbar/>
+     <CommunityHeader/>
      <Profile/>
     </div>
   );

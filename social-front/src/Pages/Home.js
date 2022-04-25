067c848dd3b4a6ed7a935eefca351e4867bf6a3f
DonKorleone <korlevice@gmail.com>
2022-04-25 20:16:23 +0200

    Dodan Pages i Home.js, Updatean app.js

--- social-front/src/Pages/Home.js
+++ social-front/src/Pages/Home.js
@@ -26,12 +26,8 @@ function App() {
      <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={3}>
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid item xs={12} sm={3}>
             </Grid>
           </Grid>
         </Container>

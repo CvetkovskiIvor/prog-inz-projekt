f00fed0fed8f1851bbb237881b0eba77816948a9
CvetkovskiIvor <cvetkovskiivor@gmail.com>
2022-06-06 14:23:58 +0200

    rijeseni bugovi

--- social-front/src/api/index.js
+++ social-front/src/api/index.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 
 const API = axios.create({baseURL: 'http://localhost:5000'});
 
-<<<<<<< HEAD
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
@@ -14,7 +13,6 @@ API.interceptors.request.use((req) => {
 
     return req;
 });
-=======
 export const fetchPosts = () => axios.get(postUrl);
 export const fetchPostsByCreator = (username) => axios.get(`${postUrl}/search?creator=${username}`);
 export const createPost = (newPost) => axios.post(postUrl, newPost);
@@ -24,15 +22,6 @@ export const disLikePost = (id) => axios.patch(`${postUrl}/${id}/disLikePost`);
 export const fetchProfiles = () => axios.get(profileUrl);
 export const fetchProfilesByURL = (URL) => axios.get(`${profileUrl}/search?username=${URL}`);
 export const createProfile = (newProfile) => axios.post(profileUrl, newProfile);
->>>>>>> profile-page
-
-export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
-export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-export const disLikePost = (id) => API.patch(`/posts/${id}/disLikePost`);
-
-export const fetchProfiles = () => API.get('/profiles');
-export const createProfile = (newProfile) => API.post('/profiles', newProfile);
 
 export const fetchCommunities = () => API.get('/communities');
 export const createCommunity = (newCommunity) => API.post('/communities', newCommunity);
